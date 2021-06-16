caaf9338500edffac3f9b985c0ebb603dbdc1f48
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-16 19:26:45 -0400

    created api.js under routes

---
+++ Develop/routes/api.js
@@ -0,0 +1,6 @@
+const userModel = require("./userModel.js");
+const express = require("express");
+const router = express.Router("express");
+const mongoose = require("mongoose");
+
+module.exports = router;
