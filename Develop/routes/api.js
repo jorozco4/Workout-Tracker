bd757594cfefead8e538e336c7cc37002bc32599
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-18 18:56:56 -0400

    added router.get to api.js and cleaned up code

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -4,30 +4,41 @@ const router = express.Router("express");
 const mongoose = require("mongoose");
 const Workout = require("../../models/workout");
 
-router.post("/api/workouts", ({body}, res) => 
-workout.create({body})
-.then(dbWorkout) => {
-    res.json(dbWorkout)
-})
-.catch(({message})) => {
-    console.log(message);
-    
-};
-
-
-router.put("/api/workouts/:id", ({body,params}, res) => {
-    console.log(body)
-    Workout.findByIdAndUpdate(params.id, { $push:{exercises:body}}, {new:true})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+router.post("/api/workouts", ({ body }, res) => {
+  workout
+    .create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(({message})) => {
-        console.log(message)
-    };
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log(body);
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-
-
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(10)
+    .then((dbWorkout) => {
+      res.json(dwWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
