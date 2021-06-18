ed36ed2a925ebb119dba46dd0786dbd1bd73802d
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-18 18:45:17 -0400

    added router put to api.js

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,20 +2,32 @@ const workout = require("../models/workout");
 const express = require("express");
 const router = express.Router("express");
 const mongoose = require("mongoose");
+const Workout = require("../../models/workout");
 
 router.post("/api/workouts", ({body}, res) => 
-workout.creat({})
+workout.create({body})
 .then(dbWorkout) => {
     res.json(dbWorkout)
 })
 .catch(({message})) => {
     console.log(message);
     
+};
 
-    });
+
+router.put("/api/workouts/:id", ({body,params}, res) => {
+    console.log(body)
+    Workout.findByIdAndUpdate(params.id, { $push:{exercises:body}}, {new:true})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(({message})) => {
+        console.log(message)
+    };
 });
 
-router.put("/api/workouts/:id", ({params, body}, res) =>
-console.log("PARAMS", body, params);
+
+
+
 
 module.exports = router;
