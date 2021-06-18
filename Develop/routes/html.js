0a874689ef154874a4f9a875d1044292a78130da
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-18 19:42:54 -0400

    created html.js routes with a bit of start code

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const path = rquire("path");
+
+module.export = funciton (app) {
+    app.get("/", (req,res)=> {
+        res.sendFile(path.join(_dirname,"../public/index.html"));
+    });
+
