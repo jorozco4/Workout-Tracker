954d1f76a35fb85a2dc564d5d4f8c631587d6213
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-19 16:26:16 -0400

    added rquire api routes to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -18,14 +18,7 @@ mongoose.connect(process.envMONGODB_URI || "mongodb://localhose/userdb", {
   useNewURLParser: true,
 });
 
-app.post("/submit", ({ body }, res) => {
-  User.create(body)
-    .then((dbUser) => {
-      res.json(dbUser);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
