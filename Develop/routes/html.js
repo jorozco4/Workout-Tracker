e4c519f1dce60e4e4f14ebb5263fd5afec58830e
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-19 09:52:34 -0400

    added app.getcode to html.js file under routes

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -6,3 +6,12 @@ module.export = funciton (app) {
         res.sendFile(path.join(_dirname,"../public/index.html"));
     });
 
+    app.get("/exercise", (res, res)=>{
+        res.sendFile(path.join(_dirname, "..//public/exercise.html"));
+    });
+
+    app.get("/stats", (req,res) => {
+        res.sendFile(path.join(_dirname,"../public/stats.html"));
+
+    });
+};
