850f1d7235f7b97bf61a684d013594f96edcf769
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-20 20:52:45 -0400

    reorgainzed html routes and apiroutes under routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,43 @@
+const Workout = require("../models/workout");
+
+module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts/range", function (req, res) {
+    Workout.create({})
+      .then((dbWorkout) => res.json(dbWorkout))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.get("/api/workouts/range", function (req, res) {
+    Workout.find()
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbWorkout) => res.json(dbWorkout))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+// const logger = require("morgan");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require();
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewURLParser: true,
+});
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}!`);
+});
